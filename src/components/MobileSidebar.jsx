347f8b4a043b656781bf7c09433934517837d745
p-prince-n <pp2358296@gmail.com>
2025-12-27 16:55:33 +0530

    changing the sidebar section

--- src/components/MobileSidebar.jsx
+++ src/components/MobileSidebar.jsx
@@ -22,27 +22,24 @@ import { NavLink } from "react-router-dom";
 
 
 const menuItems = [
-    { label: "Dashboard", icon: LayoutDashboard, path: "/" },
-    { label: "Projects", icon: Folder, path: "/projects" },
-    { label: "Tasks", icon: CheckSquare, path: "/tasks" },
-    { label: "Chat", icon: MessageCircle, path: "/chat" },
-    { label: "Meetings", icon: Calendar, path: "/meetings" },
-    { label: "Documents", icon: FileText, path: "/documents" },
-    { label: "Complaints", icon: AlertTriangle, path: "/complaints" },
-    { label: "Notice", icon: Megaphone, path: "/notice" },
-    { label: "Attendance", icon: Clock, path: "/attendance" },
-    { label: "Leave", icon: Umbrella, path: "/leave" },
-    { label: "Performance", icon: BarChart2, path: "/performance" },
-    { label: "Integrations", icon: Link2, path: "/integrations" },
-    { label: "Settings", icon: Settings, path: "/settings" },
+    { label: "Dashboard", icon: LayoutDashboard, path: "/", count: 0 },
+    { label: "Projects", icon: Folder, path: "/projects", count: 0 },
+    { label: "Chat", icon: MessageCircle, path: "/chat", count: 10 },
+    { label: "Meetings", icon: Calendar, path: "/meetings", count: 2 },
+    { label: "Documents & Reports", icon: FileText, path: "/documents", count: 2 },
+    { label: "Complaints", icon: AlertTriangle, path: "/complaints", count: 0 },
+    { label: "Notice", icon: Megaphone, path: "/notice", count: 3 },
+    { label: "Attendance & Leave", icon: Clock, path: "/attendance", count: 0 },
+    { label: "Settings", icon: Settings, path: "/settings", count: 0 },
 ];
+
 
 
 export default function MobileSidebar({ open, setOpen }) {
     const { isDark } = useThemeStore()
-    const logOutHandle=()=>{
+    const logOutHandle = () => {
         console.log('LogOut SUccessfully');
-        
+
     }
 
     return (
@@ -61,7 +58,7 @@ export default function MobileSidebar({ open, setOpen }) {
             <aside data-theme={isDark ? "dark" : "light"}
                 className={`
     bg-[#F8F8F8] dark:bg-[#2E2F2F]
-    w-[230px] lg:w-[240px]
+    w-[280px] lg:w-[280px]
     h-screen flex flex-col
     fixed lg:static
     top-0 left-0 z-100  border-r border-[#E0DDDD] dark:border-[#575757]
@@ -87,19 +84,38 @@ export default function MobileSidebar({ open, setOpen }) {
                             <NavLink
                                 key={item.label}
                                 to={item.path}
-                                onClick={() => setOpen(false)} // close on mobile
+                                onClick={() => setOpen(false)}
                                 className={({ isActive }) =>
-                                    `flex items-center gap-3 px-3 py-2 rounded-lg text-xl cursor-pointer
-           text-black dark:text-[#F8F8F8]
-           transition-colors
-           ${isActive
-                                        ? "bg-[#EDEDED] dark:bg-[#242424] font-medium"
+                                    `flex items-center justify-between px-3 py-2 rounded-lg text-xl cursor-pointer
+     text-black dark:text-[#F8F8F8]
+     transition-colors
+     ${isActive
+                                        ? "bg-[#2457C529] dark:bg-[#73FBFD]/10 font-medium"
                                         : "hover:bg-gray-100 dark:hover:bg-[#575757]"
                                     }`
                                 }
                             >
-                                <Icon size={20} />
-                                {item.label}
+
+                                <div className="flex items-center gap-3">
+                                    <Icon size={20} />
+                                    <span className="text-lg">{item.label}</span>
+                                </div>
+
+                                {/* Notification Badge */}
+                                {item.count > 0 && (
+                                    <span
+                                        className="
+      size-5
+      flex items-center justify-center
+      rounded-full text-xs font-semibold
+      bg-[#5361EB] text-white dark:text-[#000000]
+      dark:bg-[#73FBFD]
+    "
+                                    >
+                                        {item.count}
+                                    </span>
+                                )}
+
                             </NavLink>
                         );
                     })}
@@ -107,10 +123,10 @@ export default function MobileSidebar({ open, setOpen }) {
 
                 <div className="flex flex-col gap-5 px-4 py-4">
                     <div className="h-px w-full bg-[#E0DDDD] dark:bg-[#575757]" />
-                    <button onClick={()=>logOutHandle()}  className="flex cursor-pointer items-center justify-center gap-5 w-full">
+                    <button onClick={() => logOutHandle()} className="flex cursor-pointer items-center justify-center gap-5 w-full">
                         <LogOut className="size-6 text-[#FF0000]" />
                         <h2 className="text-[#FF0000] text-xl font-semibold" >Log Out</h2>
-                        
+
                     </button>
                 </div>
             </aside>
