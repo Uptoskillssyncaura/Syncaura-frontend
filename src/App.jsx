53e637e6ef6a30fa312d75a462f2af5c12a12a05
pruthweesh <pruthweesh2006@gmail.com>
2026-01-17 20:16:07 +0530

    Implemented FlowbitMeetingPage integration and Meetings routing

--- src/App.jsx
+++ src/App.jsx
@@ -3,22 +3,33 @@ import MainLayout from "./layouts/MainLayout";
 
 import Projects from "./pages/Projects";
 import Tasks from "./pages/Tasks";
-import Meetings from "./pages/CurrentMeet";
+import FlowbitMeetingPage from "./pages/Meetings";
 import Chat from "./pages/Chat";
 import Documents from "./pages/Documents";
-import Attendance from "./pages/Attendance";
 import UserDashboard from "./pages/UserDashboard";
 import Dashboard from "./pages/Dashboard";
 import SignIn from "./pages/SignIn";
 import SignUp from "./pages/SignUp";
 import Topbar from "./components/userdashboard/Topbar/Topbar"
 import Header from "./components/Meeting/Header/Header";
+import MobileSidebar from "./components/MobileSidebar";
+import Sidebar from "./components/userdashboard/Sidebar/Sidebar";
+import Complaints from "./pages/Complaints";
+import AttendanceLeave from "./pages/AttendanceLeave";
+import Notice from "./pages/Notice";
+import Settings from "./pages/Settings";
+import Admin from "./pages/Admin";
+import Home from "./pages/Home";
+import { MediaProvider } from "./context/MediaContext";
+
 
 export default function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Dashboard />} />
+        <Route path="/normal-dashboard" element={<Dashboard />} />
+        <Route path="/" element={<Home />} />
+        <Route path="/admin" element={<Admin />} />
         <Route path="/sign-in" element={<SignIn />} />
         <Route path="/sign-up" element={<SignUp />} />
 
@@ -26,20 +37,26 @@ export default function App() {
         <Route
           path="/user-dashboard"
           element={
-            <MainLayout TopbarComponent={Topbar}>
+            <MainLayout TopbarComponent={Topbar} SideBar={Sidebar} >
               <div className="  w-full h-full">
                 <UserDashboard />
               </div>
             </MainLayout>
           }
         />
-
-        {/* Other pages with default Topbar */}
         <Route
           path="/projects"
           element={
-            <MainLayout TopbarComponent={Topbar}>
+            <MainLayout TopbarComponent={Header} SideBar={MobileSidebar}>
               <Projects />
+            </MainLayout>
+          }
+        />
+        <Route
+          path="/attendance-leave"
+          element={
+            <MainLayout TopbarComponent={Header} SideBar={MobileSidebar}>
+              <AttendanceLeave />
             </MainLayout>
           }
         />
@@ -51,7 +68,7 @@ export default function App() {
             </MainLayout>
           }
         />
-        <Route 
+       <Route 
         path = "/meetings"
         elements={
           <MainLayout TopbarComponent={Header}>
@@ -71,30 +88,50 @@ export default function App() {
     </MediaProvider>
   }
 />
+
+
         <Route
           path="/chat"
           element={
-            <MainLayout TopbarComponent={Topbar}>
+            <MainLayout TopbarComponent={Header} SideBar={MobileSidebar} >
               <Chat />
+            </MainLayout>
+          }
+        />
+        <Route
+          path="/notice"
+          element={
+            <MainLayout TopbarComponent={Header} SideBar={MobileSidebar} >
+              <Notice />
             </MainLayout>
           }
         />
         <Route
           path="/documents"
           element={
-            <MainLayout TopbarComponent={Topbar}>
+            <MainLayout TopbarComponent={Header} SideBar={MobileSidebar} >
               <Documents />
             </MainLayout>
           }
         />
+
         <Route
-          path="/attendance"
+          path="/complaints"
           element={
-            <MainLayout TopbarComponent={Topbar}>
-              <Attendance />
+            <MainLayout TopbarComponent={Header} SideBar={MobileSidebar} >
+              <Complaints />
             </MainLayout>
           }
         />
+        <Route
+          path="/settings"
+          element={
+            <MainLayout TopbarComponent={Header} SideBar={MobileSidebar} >
+              <Settings />
+            </MainLayout>
+          }
+        />
+        
       </Routes>
     </BrowserRouter>
   );
